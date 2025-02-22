87761cc084db08092fe41f3972b05848509a0cf7
henri ouma <147258392+Henri2611@users.noreply.github.com>
2025-02-22 20:09:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,36 @@
 
 // sayHello( greetUser);
 
+//callback with arguments
+// const greetUser = (name, callback) => {
+//   callback(name);
+// };
+
+// const displayMessage = (name) => {
+//   console.log(`Welcome,${name} !`);
+// };
+
+// greetUser("Henri", displayMessage);
+
+// //callback with a delay
+
+// const delayedMessage = (message, callback) => {
+//   console.log("Processing...");
+//   setTimeout(() => callback(message), 200);
+// };
+
+// const showMessage = (message) => {
+//   console.log(`Message ${message}`);
+// };
+
+// delayedMessage("Hello, after 2 seconds!", showMessage);
+
+// //using callbacks with map()
+
+// const doubleNumbers = (arr, callback) => {
+//   return arr.map(callback);
+// };
+
+// const double = (num) => num + 2;
+
+// console.log(doubleNumbers([1, 2, 3], double));
